0b091fea790262dbb6e0d32628978e45757e211c
Yuriy Dzyuban <yuidzyuban@avito.ru>
2023-08-02 11:42:43 +0300

    OBS-394 * added required parameter `valuesFrom` * tests

--- expr/functions/groupLeftByNodes/function.go
+++ expr/functions/groupLeftByNodes/function.go
@@ -29,9 +29,16 @@ func (f *groupLeftByNodes) Description() map[string]types.FunctionDescription {
 		"groupLeftByNodes": {
 			Description: `Performs operation similar to "group_left" in prometheus. Used on non-tagged metrics only.
 Each series from "seriesList1" is matched with each series from "seriesList2" based on values at specified nodes.
-Name of matched series from the second list is appended to respective names from the first list
+Name of matched series from the second list is appended to respective name from the first list.
+Following options for "valuesFrom" parameter are available:
+  * "default" - values in result will be taken from the first series. If you need values from the second series,
+	just swap "seriesList1" and "seriesList2" arguments.
+  * "common" - values in result also will be taken from the first series, but for each absent value from the
+	second series, respective value from the first series will be absent as well. For example, two metrics have matched:
+	"aaa.bbb.ccc" with values [1, 2, 3, 4, 5] from the first list and "aaa.bbb.ddd" with values [5, null, 3, 2, null] 
+	from the second list, therefore result metric will be "aaa.bbb.ccc.aaa.bbb.ddd" with values [1, null, 3, 4, null].
 `,
-			Function: "groupLeftByNodes(seriesList1, seriesList2, 0, 1, -2, ...)",
+			Function: "groupLeftByNodes(seriesList1, seriesList2, 'default', 0, 1, -2, ...)",
 			Group:    "Transform",
 			Module:   "graphite.render.functions",
 			Name:     "groupLeftByNodes",
@@ -47,6 +54,13 @@ Name of matched series from the second list is appended to respective names from
 					Type:     types.SeriesList,
 				},
 				{
+					Name:     "valuesFrom",
+					Required: false,
+					Type:     types.String,
+					Default:  types.NewSuggestion(valuesDefault),
+					Options:  []string{valuesDefault, valuesCommon},
+				},
+				{
 					Multiple: true,
 					Required: true,
 					Name:     "nodes",
@@ -56,6 +70,11 @@ Name of matched series from the second list is appended to respective names from
 		},
 	}
 }
+
+const (
+	valuesDefault = "default"
+	valuesCommon  = "common"
+)
 
 func (f *groupLeftByNodes) Do(e parser.Expr, from, until int32, values map[parser.MetricRequest][]*types.MetricData) (results []*types.MetricData, err error) {
 	seriesList1, err := helper.GetSeriesArg(e.Args()[0], from, until, values)
@@ -67,27 +86,58 @@ func (f *groupLeftByNodes) Do(e parser.Expr, from, until int32, values map[parse
 		return nil, err
 	}
 
-	nodes, err := e.GetIntArgs(2)
+	valuesFrom, err := e.GetStringNamedOrPosArgDefault("valuesFrom", 2, valuesDefault)
+	if err != nil {
+		return nil, err
+	}
+	valuesFrom = strings.ToLower(valuesFrom)
+	if valuesFrom != valuesDefault && valuesFrom != valuesCommon {
+		return nil, fmt.Errorf(
+			"unknown %q value: only %q and %q are allowed",
+			valuesFrom, valuesDefault, valuesCommon,
+		)
+	}
+
+	nodes, err := e.GetIntArgs(3)
 	if err == parser.ErrMissingArgument {
 		return nil, fmt.Errorf("at least one node must be specified")
+	}
+
+	var mergeMetricData func(series1, series2 *types.MetricData) *types.MetricData
+	switch strings.ToLower(valuesFrom) {
+	case valuesDefault:
+		mergeMetricData = mergeMetricDataDefault
+	case valuesCommon:
+		mergeMetricData = mergeMetricDataCommon
+	default:
+		return nil, fmt.Errorf(
+			"unknown valuesFrom value %q: only %q and %q are allowed",
+			valuesFrom, valuesDefault, valuesCommon,
+		)
 	}
 
 	grouped2 := groupByKey(seriesList2, nodes)
 	results = make([]*types.MetricData, 0, len(seriesList1))
 	for _, series1 := range seriesList1 {
 		key := safeKeyByNodes(series1.Name, nodes)
-		list2, ok := grouped2[key]
-		if !ok {
-			continue
-		}
-
-		for _, data2 := range list2 {
-			data1 := *series1
-			data1.Name = fmt.Sprintf("%s.%s", data1.Name, data2.Name)
-			results = append(results, &data1)
+		for _, series2 := range grouped2[key] {
+			results = append(results, mergeMetricData(series1, series2))
 		}
 	}
 	return results, nil
+}
+
+func copyMetricData(md *types.MetricData) *types.MetricData {
+	data := *md
+	result := &data
+
+	result.IsAbsent = make([]bool, len(md.IsAbsent))
+	copy(result.IsAbsent, md.IsAbsent)
+
+	result.Values = make([]float64, len(md.Values))
+	copy(result.Values, md.Values)
+
+	return result
 }
 
 func groupByKey(list []*types.MetricData, keyNodes []int) map[string][]*types.MetricData {
@@ -99,6 +149,30 @@ func groupByKey(list []*types.MetricData, keyNodes []int) map[string][]*types.Me
 	return result
 }
 
+func mergeMetricDataCommon(series1, series2 *types.MetricData) *types.MetricData {
+	result := copyMetricData(series1)
+	result.Name = fmt.Sprintf("%s.%s", series1.Name, series2.Name)
+
+	for i := range result.Values {
+		if i >= len(result.IsAbsent) || i >= len(series2.IsAbsent) {
+			continue
+		}
+
+		if !result.IsAbsent[i] && series2.IsAbsent[i] {
+			result.Values[i] = 0
+			result.IsAbsent[i] = true
+		}
+	}
+
+	return result
+}
+
+func mergeMetricDataDefault(series1, series2 *types.MetricData) *types.MetricData {
+	result := copyMetricData(series1)
+	result.Name = fmt.Sprintf("%s.%s", series1.Name, series2.Name)
+	return result
+}
+
 const sep = "."
 
 func safeKeyByNodes(metric string, keyNodes []int) string {
@@ -106,13 +180,16 @@ func safeKeyByNodes(metric string, keyNodes []int) string {
 	partsQty := len(parts)
 
 	result := strings.Builder{}
-	for _, node := range keyNodes {
+	for i, node := range keyNodes {
 		if node >= 0 && node < partsQty {
 			result.WriteString(parts[node])
 		} else if node < 0 && node >= -partsQty {
 			result.WriteString(parts[partsQty+node])
 		}
-		result.WriteString(sep)
+
+		if i != len(keyNodes)-1 {
+			result.WriteString(sep)
+		}
 	}
 	return result.String()
 }
--- expr/functions/groupLeftByNodes/function_test.go
+++ expr/functions/groupLeftByNodes/function_test.go
@@ -2,20 +2,23 @@ package groupLeftByNodes
 
 import (
 	"fmt"
-	"github.com/go-graphite/carbonapi/expr/types"
-	"github.com/go-graphite/carbonapi/pkg/parser"
+	"math"
 	"testing"
 	"time"
 
 	"github.com/go-graphite/carbonapi/expr/helper"
 	"github.com/go-graphite/carbonapi/expr/metadata"
+	"github.com/go-graphite/carbonapi/expr/types"
+	"github.com/go-graphite/carbonapi/pkg/parser"
 	"github.com/go-graphite/carbonapi/tests"
 )
 
 var (
+	nan   = math.NaN()
 	now32 = int32(time.Now().Unix())
 	vals1 = []float64{1.1, 1.2, 1.3, 1.4, 1.5}
-	vals2 = []float64{2.1, 2.2, 2.3, 2.4, 2.5}
+	vals2 = []float64{2.1, math.NaN(), 2.3, math.NaN(), math.NaN()}
+	vals3 = []float64{1.1, math.NaN(), 1.3, math.NaN(), math.NaN()}
 )
 
 func init() {
@@ -31,8 +34,34 @@ func init() {
 func Test_groupLeftByNodes_Do(t *testing.T) {
 	testCases := []tests.EvalTestItem{
 		{
+			E: parser.NewExpr("groupLeftByNodes", "*.a", "*.b", parser.ArgValue("default"), 1, -1),
+			M: map[parser.MetricRequest][]*types.MetricData{
+				{"*.a", 0, 1}: {
+					types.MakeMetricData("a11.xxx.yyy", vals1, 1, now32),
+					types.MakeMetricData("a21.xxx.yyy", vals1, 1, now32),
+					types.MakeMetricData("a31.xxx.yyy", vals1, 1, now32),
+					types.MakeMetricData("a41.xxx.yyy", vals1, 1, now32),
+					types.MakeMetricData("a51.zzz.ttt", vals1, 1, now32),
+					types.MakeMetricData("a61.yyy.xxx", vals1, 1, now32),
+				},
+				{"*.b", 0, 1}: {
+					types.MakeMetricData("a12.xxx.yyy", vals2, 1, now32),
+					types.MakeMetricData("a22.zzz.ttt", vals2, 1, now32),
+					types.MakeMetricData("a32.kkk.mmm", vals2, 1, now32),
+				},
+			},
+			Want: []*types.MetricData{
+				types.MakeMetricData("a11.xxx.yyy.a12.xxx.yyy", vals1, 1, now32),
+				types.MakeMetricData("a21.xxx.yyy.a12.xxx.yyy", vals1, 1, now32),
+				types.MakeMetricData("a31.xxx.yyy.a12.xxx.yyy", vals1, 1, now32),
+				types.MakeMetricData("a41.xxx.yyy.a12.xxx.yyy", vals1, 1, now32),
+				types.MakeMetricData("a51.zzz.ttt.a22.zzz.ttt", vals1, 1, now32),
+			},
+			WantError: nil,
+		},
+		{
 			// empty nodes list - incorrect
-			E: parser.NewExpr("groupLeftByNodes", "*.a", "*.b"),
+			E: parser.NewExpr("groupLeftByNodes", "*.a", "*.b", parser.ArgValue("default")),
 			M: map[parser.MetricRequest][]*types.MetricData{
 				{"*.a", 0, 1}: {},
 				{"*.b", 0, 1}: {},
@@ -41,7 +70,17 @@ func Test_groupLeftByNodes_Do(t *testing.T) {
 			WantError: fmt.Errorf("at least one node must be specified"),
 		},
 		{
-			E: parser.NewExpr("groupLeftByNodes", "*.a", "*.b", 1),
+			// unknown parameter "valuesFrom"
+			E: parser.NewExpr("groupLeftByNodes", "*.a", "*.b", parser.ArgValue("fgsfds")),
+			M: map[parser.MetricRequest][]*types.MetricData{
+				{"*.a", 0, 1}: {},
+				{"*.b", 0, 1}: {},
+			},
+			Want:      nil,
+			WantError: fmt.Errorf("unknown \"fgsfds\" value: only \"default\" and \"common\" are allowed"),
+		},
+		{
+			E: parser.NewExpr("groupLeftByNodes", "*.a", "*.b", parser.ArgValue("default"), 1),
 			M: map[parser.MetricRequest][]*types.MetricData{
 				{"*.a", 0, 1}: {
 					types.MakeMetricData("a11.join.c11", vals1, 1, now32),
@@ -68,28 +107,17 @@ func Test_groupLeftByNodes_Do(t *testing.T) {
 			WantError: nil,
 		},
 		{
-			E: parser.NewExpr("groupLeftByNodes", "*.a", "*.b", 1, -1),
+			E: parser.NewExpr("groupLeftByNodes", "*.a", "*.b", parser.ArgValue("common"), 1),
 			M: map[parser.MetricRequest][]*types.MetricData{
 				{"*.a", 0, 1}: {
-					types.MakeMetricData("a11.xxx.yyy", vals1, 1, now32),
-					types.MakeMetricData("a21.xxx.yyy", vals1, 1, now32),
-					types.MakeMetricData("a31.xxx.yyy", vals1, 1, now32),
-					types.MakeMetricData("a41.xxx.yyy", vals1, 1, now32),
-					types.MakeMetricData("a51.zzz.ttt", vals1, 1, now32),
-					types.MakeMetricData("a61.yyy.xxx", vals1, 1, now32),
+					types.MakeMetricData("xxx.aaa", vals1, 1, now32),
 				},
 				{"*.b", 0, 1}: {
-					types.MakeMetricData("a12.xxx.yyy", vals2, 1, now32),
-					types.MakeMetricData("a22.zzz.ttt", vals2, 1, now32),
-					types.MakeMetricData("a32.kkk.mmm", vals2, 1, now32),
+					types.MakeMetricData("yyy.aaa", vals2, 1, now32),
 				},
 			},
 			Want: []*types.MetricData{
-				types.MakeMetricData("a11.xxx.yyy.a12.xxx.yyy", vals1, 1, now32),
-				types.MakeMetricData("a21.xxx.yyy.a12.xxx.yyy", vals1, 1, now32),
-				types.MakeMetricData("a31.xxx.yyy.a12.xxx.yyy", vals1, 1, now32),
-				types.MakeMetricData("a41.xxx.yyy.a12.xxx.yyy", vals1, 1, now32),
-				types.MakeMetricData("a51.zzz.ttt.a22.zzz.ttt", vals1, 1, now32),
+				types.MakeMetricData("xxx.aaa.yyy.aaa", vals3, 1, now32),
 			},
 			WantError: nil,
 		},
